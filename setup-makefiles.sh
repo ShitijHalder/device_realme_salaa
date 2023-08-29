cd9e445f79e0615ee61ea7487b0a172c862d3881
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:58:31 -0500

    salaa: Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,6 +24,15 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/realme/salaa",
+		"hardware/mediatek",
+		"hardware/mediatek/libmtkperf_client",
+		"hardware/oplus",
+EOF
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
