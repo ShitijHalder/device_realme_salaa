7e191500bc96f2f7819f486618715230c85a08d6
SHITIJ.arc <shitijhalder4@gmail.com>
2024-06-13 19:06:46 +0530

    salaa: Automate build prerequisites
    Automate built prerequisites by adding a script!

---
+++ vendorsetup.sh
@@ -0,0 +1,20 @@
+# Cloning required repositories for making rum:
+
+# Kernel and Vendor Source:
+git clone https://github.com/ShitijHalder/kernel_realme_salaa kernel/realme/salaa
+git clone https://github.com/ShitijHalder/vendor_realme_salaa vendor/realme/salaa
+
+# Hardware of Mediatek and Oplus:
+git clone https://github.com/ShitijHalder/android_hardware_mediatek hardware/mediatek
+git clone https://github.com/ShiChiyooo/platform_hardware_oplus hardware/oplus
+
+# Mediatek SEPolicy VNDR:
+git clone https://github.com/ShitijHalder/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+
+# Lineage SEPolicy:
+git clone https://github.com/LineageOS/android_device_lineage_sepolicy device/lineage/sepolicy
+
+# Basic Call Recorder (BCR):
+git clone https://github.com/kenway214/vendor_bcr.git vendor/bcr
+
+
