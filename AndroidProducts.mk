c73f3d145eec645b528c3b02eea8860e4eee73f4
SHITIJ.arc <shitijhalder4@gmail.com>
2024-06-23 19:28:57 +0530

    salaa: Bringup @ProjectBlaze
    Signed-off-by: SHITIJ.arc <shitijhalder4@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2023 The ArrowOS Project
+# Copyright (C) 2024 The AOSP - Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/everest_salaa.mk
+    $(LOCAL_DIR)/blaze_salaa.mk
 
 COMMON_LUNCH_CHOICES := \
-    everest_salaa-user \
-    everest_salaa-userdebug \
-    everest_salaa-eng
+    blaze_salaa-user \
+    blaze_salaa-userdebug \
+    blaze_salaa-eng
--- blaze_salaa.mk
+++ blaze_salaa.mk
@@ -1,9 +1,8 @@
 #
-# Copyright (C) 2023 The ArrowOS Project
+# Copyright (C) 2024 The AOSP - Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -11,30 +10,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit from device makefile.
 $(call inherit-product, device/realme/salaa/device.mk)
-PRODUCT_NAME := everest_salaa
+PRODUCT_NAME := blaze_salaa
 PRODUCT_DEVICE := salaa
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme 7/Narzo 20 Pro/Narzo 30 4G
 
-# Inherit some common Everest stuff.
-$(call inherit-product, vendor/everest/config/common_full_phone.mk)
-
-# Boot Animation Resolution
+# Inherit some common Blaze stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+# Blaze Stuff
+BLAZE_BUILD_TYPE := UNOFFICIAL
+BLAZE_MAINTAINER := SHITIJ.dev
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# Everest Maintainer Stuff
-EVEREST_BUILD_TYPE := UNOFFICIAL
-EVEREST_MAINTAINER := SHITIJ.dev
-
-# Gapps
-WITH_GAPPS := true 
-
-# Extra Stuffs
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_BLUR := true
-TARGER_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GAPPS := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
