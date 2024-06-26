50eed7d83497f65b8c7649525d340ee4035b05b5
SHITIJ.arc <shitijhalder4@gmail.com>
2024-06-26 11:55:46 +0530

    Revert "salaa: Add firmware extraction support, version: RMX2156_12_F.18"
    This reverts commit a5db6c948226829e5315051ad3c7587808feba4f.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -32,8 +32,5 @@ write_headers
 
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
-# Append the calls to firmware images
-append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
-
 # Finish
 write_footers
