c77a8c0e6769e88219daf9803a744d416c7da689
Michael Bestas <mikeioannina@gmail.com>
2024-07-27 22:34:32 +0530

    salaa: Include Lineage FCM.
    Signed-off-by: SHITIJ.arc <shitijhalder4@gmail.com>

---
+++ BoardConfigLineage.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Health
+TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/power_supply/battery/mmi_charging_enable
+
+# VINTF
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    vendor/lineage/config/device_framework_matrix.xml
