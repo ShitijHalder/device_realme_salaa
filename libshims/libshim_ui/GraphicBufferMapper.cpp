fb4598605ecebe2021a0b3034be42117b0654519
Giovanni Ricca <giovanniricca@proton.me>
2024-07-27 22:47:24 +0530

    salaa: libshims: Add android::GraphicBufferMapper::unlock
    Signed-off-by: SHITIJ.arc <shitijhalder4@gmail.com>

--- libshims/libshim_ui/GraphicBufferMapper.cpp
+++ libshims/libshim_ui/GraphicBufferMapper.cpp
@@ -19,4 +19,16 @@ status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvP
     auto* gpm = static_cast<android::GraphicBufferMapper*>(thisptr);
     return gpm->lock(handle, usage, bounds, vaddr);
 }
+
+status_t _ZN7android19GraphicBufferMapper6unlockEPK13native_handle(void* thisptr,
+                                                                   buffer_handle_t handle) {
+    android::base::unique_fd outFence;
+    auto* gpm = static_cast<android::GraphicBufferMapper*>(thisptr);
+    status_t status = gpm->unlock(handle, &outFence);
+    if (status == android::OK && outFence.get() >= 0) {
+        sync_wait(outFence.get(), -1);
+        outFence.reset();
+    }
+    return status;
+}
 }
