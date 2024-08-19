cd88bec2f27c1d877d34155483f1859013d652c7
Bruno Martins <bgcngm@gmail.com>
2024-08-19 22:47:24 +0530

    salaa: Patch  for missing GraphicBufferMapper REF: https://github.com/PixelOS-Devices/device_realme_salaa/commit/4c9cc110498e7d7920756dbc8308c4c360fb31be Vendor side commit for salaa: https://github.com/ShitijHalder/vendor_realme_salaa/commit/3cb74ca9715c79a842c84451ead58f6c2aaf4964
    Signed-off-by: SHITIJ.arc <shitijhalder4@gmail.com>

--- device.mk
+++ device.mk
@@ -188,6 +188,9 @@ PRODUCT_BOOT_JARS += \
     oplus-framework \
     oplus-framework-telephony
 
+PRODUCT_PACKAGES += \
+    libui_shim
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
 
