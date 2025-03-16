0a5abdfc888712ced39fb4d2b3a8f7d823ede032
Georg Veichtlbauer <georg@vware.at>
2025-03-16 08:44:22 +0530

    salaa: Remove unneeded NFC packages
    * NfcNci and SecureElement are built by handheld_system.mk
    
    Change-Id: I83f686b67778634b584a58d8bae95d237e6dbad1

--- device.mk
+++ device.mk
@@ -174,8 +174,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
-    NfcNci \
-    SecureElement \
     Tag
 
 PRODUCT_COPY_FILES += \
