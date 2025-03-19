e2ad9ce33c7b187393cac05d257f1d171c9f4001
Debayan Kar <kardebayan3@gmail.com>
2025-03-19 21:42:44 +0530

    fixup! salaa: Link audio modules against prebuilt libalsautils.so

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,9 @@ function blob_fixup {
         vendor/etc/init/android.hardware.media.c2@1.2-mediatek.rc)
             sed -i 's/@1.2-mediatek/@1.2-mediatek-64b/g' "${2}"
             ;;
+        vendor/lib/hw/audio.primary.mt6785.so)
+             "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v31.so" "${2}"
+             ;;
         vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
             grep -q "libcamera_metadata_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_metadata_shim.so" "${2}"
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
